15fb89cab87fd3b4ba7c54c93e6cc2f29ed68157
hrpisme <104566771+hrpisme@users.noreply.github.com>
2022-05-17 23:45:49 +0800

    Add files via upload

---
+++ MFCI.m
@@ -0,0 +1,79 @@
+clear;clc;
+load('net.mat.mat');
+load('net.mat.mat');
+Td=20;tao=0.01;rou=0.5;
+
+%   6R机器人模型，名称modified puma560。
+%   定义机器人 a--连杆长度，d--连杆偏移量
+    a2=0.4318;a3=0.02032;d2=0.14909;d4=0.43307;
+%		     thetai    di      ai-1        alphai-1               关节变量取值范围，角度制    
+    L1 = Link([pi/2    0       0               0], 'modified');%-160，160
+    L2 = Link([0       d2      0           -pi/2], 'modified');%-225，45
+    L3 = Link([-pi/2   0       a2              0], 'modified');%-45，225
+    L4 = Link([0       d4      a3          -pi/2], 'modified');%-110，170
+    L5 = Link([0       0  	   0            pi/2], 'modified');%-100，100
+    L6 = Link([0       0       0           -pi/2], 'modified');%-200，200
+    robot = SerialLink([L1,L2,L3,L4,L5,L6]);
+    robot.name = 'modified puma560';
+%   robot.display();
+%	robot.teach();
+%   robot.plot([0 0 0 0 0 0]);
+%   图像生成
+%     for n = 1:Td/tao
+%         xd(1,n)=0.4*cos(2*pi*n*tao/Td)^3+0.01;% 行矩阵
+%         yd(1,n)=0.4*sin(2*pi*n*tao/Td)^3;
+%         zd(1,n)=0.4*sin(2*pi*n*tao/Td)^3;
+%     end
+    %   五角星
+    for n = 1:Td/tao
+        xd(1,n)=0.2*rou*cos(6*pi*n*tao/Td)+0.5*rou*cos(4*pi*n*tao/Td)-0.7*rou+0.5;% 行矩阵
+        yd(1,n)=-0.50;
+        zd(1,n)=0.2*rou*sin(6*pi*n*tao/Td)-0.5*rou*sin(4*pi*n*tao/Td);
+    end
+
+    pd=[xd;yd;zd];
+    plot3(pd(1,:),pd(2,:),pd(3,:));
+    hold on;
+    %初始化参数
+    Win=rand(400,15)-0.5;% 范围【-0.5，0.5】 400x15
+    W=rand(400,400);
+    W=W*0.8/vrho(W);% 谱半径0.8  vrho()计算谱半径
+    Wout=zeros(3,400);% Wout(t0)=0
+    x=zeros(400,1);% x(t0)=0;
+    pa=[1 0 0 0.5;0 1 0 -0.5;0 0 1 0;0 0 0 1];
+    thetai=Inverse_kinematics(pa);
+    thetai=thetai.';% 初始状态thetai
+    delta_thetai=[0 0 0 0 0 0].';% 初始状态delta_thetai
+    pa=kinematics(thetai.');% 正运动学解
+    pa=SE3(pa);
+    pa=transl(pa);% 提取出位置信息
+    pa=pa.';% 初始状态下的pa
+for i=1:20
+    for n=1:Td/tao
+        if mod(n,10)==0
+            robot.plot(thetai.');
+        end
+        if mod(n,10)==0
+            plot3(pa(1,1),pa(2,1),pa(3,1),'r.');
+        end
+        u=[pd(:,n);thetai;delta_thetai];% new u,new pd,old tehtai,old delta_thetai
+        x=sigmod(Win*u+W*x);% new x,new u, old x
+        p=x;% new p,new x
+        max(max(abs(Wout*p)))
+        y=tanh(Wout*p);% new y,old Wout,new p
+        pd_adjust=pd(:,n)+y;% all new
+        old_thetai=thetai;% 记录old thetai
+        thetai=thetai+net([pd_adjust;old_thetai;pa]);% new thetai,new pd_adjust,old thetai,old pa
+        delta_thetai=thetai-old_thetai;% new delta_thetai,new thetai,ole thetai
+        pa=kinematics(thetai.');% new pa,new thetai
+        pa=SE3(pa);
+        pa=transl(pa);% 提取出位置信息
+        pa=pa.';% new pa
+        e=pd(:,n)-pa;% new e,new pd,new pa
+        delta_Wout=e*p.';% new deltai_Wout, new e,new p
+        Wout=Wout+0.001*delta_Wout;% new Wout,old Wout,new delta_Wout
+    end
+    clf;
+    plot3(pd(1,:),pd(2,:),pd(3,:));
+    hold on;
+end
---
+++ MFCI_MLP.m
@@ -0,0 +1,68 @@
+clear;clc;
+%   6R机器人模型，名称modified puma560。
+%   定义机器人 a--连杆长度，d--连杆偏移量
+    a2=0.4318;a3=0.02032;d2=0.14909;d4=0.43307;
+%		     thetai    di      ai-1        alphai-1               关节变量取值范围，角度制    
+    L1 = Link([pi/2    0       0               0], 'modified');%-160，160
+    L2 = Link([0       d2      0           -pi/2], 'modified');%-225，45
+    L3 = Link([-pi/2   0       a2              0], 'modified');%-45，225
+    L4 = Link([0       d4      a3          -pi/2], 'modified');%-110，170
+    L5 = Link([0       0  	   0            pi/2], 'modified');%-100，100
+    L6 = Link([0       0       0           -pi/2], 'modified');%-200，200
+    robot = SerialLink([L1,L2,L3,L4,L5,L6]);
+    robot.name = 'modified puma560';
+%   robot.display();
+%	robot.teach();
+%   robot.plot([0 0 0 0 0 0]);
+%   图像生成
+    for n = 1:2000
+        xd(n)=0.4*cos(2*pi*n/2000)^3;% 行矩阵
+        yd(n)=0.4*sin(2*pi*n/2000)^3;
+        zd(n)=0.4*sin(2*pi*n/2000)^3;
+    end
+    pd=[xd;yd;zd];
+    %plot3(pd(1,:),pd(2,:),pd(3,:));
+    %hold on;
+%初始化
+    pa=[1 0 0 0.5;0 1 0 0;0 0 1 0;0 0 0 1];
+    thetai=Inverse_kinematics(pa);
+    thetai=thetai.';% 初始状态thetai
+    delta_thetai=[0 0 0 0 0 0].';% 初始状态delta_thetai
+%   获取MLP训练需要的数据集
+    Nt=5000;
+    train_input=zeros(12,Nt);train_out=zeros(6,Nt);
+    for i=1:Nt
+        pa=kinematics(thetai.');
+        pa=SE3(pa);
+        pa=transl(pa);% 获得旧时刻EEF位置信息
+        pa=pa.';
+        old_thetai=thetai;% 记录旧时刻thetai
+        incre_thetai=0.01*pi*2*(rand(6,1)-0.5);% 随机产生关节角度增量并调整
+        thetai=thetai+incre_thetai;% 新thetai
+        new_pa=kinematics(thetai.');
+        new_pa=SE3(new_pa);
+        new_pa=transl(new_pa);% 获得新时刻EEF位置信息
+        new_pa=new_pa.';
+        train_input(:,i)=[new_pa;old_thetai;pa];
+        train_out(:,i)=incre_thetai;
+        
+    end
+% 训练MLP网络
+    net = fitnet(40);
+    net = train(net,train_input,train_out); 
+    view(net);
+    save('net1.mat.mat','net');
+    
+    x=net(train_input);
+    y=train_out;
+    x=x(1,:);
+    y=y(1,:);
+    plot(x,'g')
+    hold on;
+    plot(y,'r');
+
+    
+    
+    
+    
+    
