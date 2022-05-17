020c96734a7f046525811b2756c47a3dd220437d
hrpisme <104566771+hrpisme@users.noreply.github.com>
2022-05-17 22:07:04 +0800

    Add files via upload

--- MBCI.m
+++ MBCI.m
@@ -14,7 +14,7 @@
 %   robot.display();
 %	robot.teach(); %  互动
    
-    Td=20;tao=0.1;
+    Td=20;tao=0.01;
 %   目标轨迹图像生成
     xd=zeros(1,Td/tao);zd=zeros(1,Td/tao);yd=zeros(1,Td/tao);
     for n = 1:Td/tao
@@ -40,30 +40,44 @@
     pa=SE3(pa);
     pa=transl(pa);% 提取出位置信息
     pa=pa.';% 初始状态下的pa
-%  一些参数设置
+%  LIN的参数
+    h=2.2;l=0.6;
+%  开始
 for i=1:20  % 转20圈
     for n=1:Td/tao
         delta_p=pd(:,n)-pa;% new deltai_p,new pd,old pa
         u=[thetai;delta_p;delta_thetai];% new u,old thetai,new delta_p,ole delta_thetai   输入层
-        x=sigmod(Win*u+W*x);% new x,new u,old x    存储层
+        % 没有LIN的存储层
+        %x=sigmod(Win*u+W*x);% new x,new u,old x   
+        % LIN的存储层
+        x=(1-h*l)*x+h*sigmod(Win*u+W*x);
         p=x;% new p,new x    论文里给的不知道什么层，个人感觉没必要
+        % max(max(abs(Wout*p)))
         delta_thetai=tanh(Wout*p);% new delta_thetai,old Wout,new p      得到 ESN 输出
         thetai=thetai+delta_thetai;% new thetai,old thetai,new delta_thatai   得到新的thetai
-        robot.plot(thetai.');% new thetai     控制机器人
+        if mod(n,30)==0 % 图像30步一标
+            robot.plot(thetai.');% new thetai     控制机器人
+        end
         pa=kinematics(thetai.');% 正运动学解    
         pa=SE3(pa);
         pa=transl(pa);% 提取出位置信息    
         pa=pa.';% new pa     得到实际位置
-        plot3(pa(1,1),pa(2,1),pa(3,1),'r.');  % 标出实际位置
-        J=robot.jacob0(thetai.','trans');    % 雅可比矩阵
+        if mod(n,30)==0
+            plot3(pa(1,1),pa(2,1),pa(3,1),'r.');  % 标出实际位置
+        end
+        J=robot.jacob0;    % 雅可比矩阵
         % 其中前3行代表机器人末端坐标系线速度的传递比；后3行代表对手爪的角速度的传递比
         % J=[J(1,:);J(2,:);J(3,:)];
-        pinv_J=pinv(J);  % 伪逆
+        pinv_J=pinv(J);  % 伪逆 
+        % 尝试解决奇异点问题
+
+        % 找不到方法
         e=pinv_J*(pd(:,n)-pa);% new e,new pd,new pa   得到e
         delta_Wout=e*p.';   %  更新Wout
-        Wout=Wout+0.01*delta_Wout;   %  注意系数，影响很大
+        Wout=Wout+0.001*delta_Wout;   %  注意系数，影响很大，而且和tao关系很大
     end
     clf;  %清空图像，画第二轮图像
     plot3(pd(1,:),pd(2,:),pd(3,:));
     hold on;
 end
+% 有个非常严重的问题，一旦碰到奇异点，基本难以完成任务
